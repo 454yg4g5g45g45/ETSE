790466f4d0e1ce828e354d4b7d52f59a0da3e8d1
77Nowhere <johnmuja76@gmail.com>
2026-02-15 05:40:08 -0500

    ..

--- common/custom_gui/mmetr_defines.lua
+++
@@ -1,6 +0,0 @@
--- Format for overwriting define values:
---
--- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
-
--- Fixes Terra Incognita for early start dates
-NDefines.NCountry.PROVINCE_DISCOVERY_YEARS_TECHNOLOGY = 2
