78a83f47c635310c80e22d7a6491e858fa296db2
77Nowhere <johnmuja76@gmail.com>
2026-02-15 05:28:05 -0500

    ...

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ETSE
-
+# Extended Timeline: Super Expanded
+ A totally simple fork of Extended Timeline when qwetyr moves to EU5
--- common/defines/00_clientstates.lua
+++
@@ -1,5 +0,0 @@
--- Format for overwriting define values:
---
--- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
-
-NDefines.NCountry.OVERSEAS_CLIENT_STATES = 1					-- Allow overseas client states?
